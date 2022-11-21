37031142f1e60978a7d2ea37d0d92d28df781018
Sachin Kumar <cod50718@adobe.com>
2022-11-21 16:13:31 +0530

    Adding custom pagebuilder attribute to prosuct form

---
+++ Custom PageBuilder Attribute/Setup/Patch/Data/PageBilder.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Codilar\Catalog\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+class PageBilder  implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'custom',
+            [
+                'type' => 'text',
+                'frontend' => '',
+                'label' => 'Custom',
+                'input' => 'textarea',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'unique' => false,
+                'apply_to' => '',
+                'group' => 'Content',
+                'attribute_set_id' => 'Default',
+                'used_in_product_listing' => false
+
+            ]
+        );
+
+        // For whatever reason, can only set these properties with updateAttribute
+        $eavSetup->updateAttribute(
+            Product::ENTITY,
+            'custom',
+            [
+                'is_pagebuilder_enabled' => 1,
+                'is_html_allowed_on_front' => 1,
+                'is_wysiwyg_enabled' => 1
+            ]
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(\Magento\Catalog\Model\Product::ENTITY, 'custom');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
