d4871f2e5bd014d3a8757408ef612f2688b82f27
Prajwal-codilar <prajwal.j@codilar.com>
2023-06-08 12:37:14 +0530

    Pgae Biulder for the Category

---
+++ Page Builder Attribute for Category/Catalog/Setup/Patch/Data/PageBilder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Codilar\Catalog\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\TestFramework\Event\Magento;
+
+class PageBilder implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'custom',
+            [
+                'type' => 'text',
+                'label' => 'Custom',
+                'input' => 'textarea',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'wysiwyg_enabled' => true,
+                'is_html_allowed_on_front' => true,
+                'group'    => 'Seo Content',
+            ]
+        );
+
+        // For whatever reason, can only set these properties with updateAttribute
+        $eavSetup->updateAttribute(
+            Category::ENTITY,
+            'custom',
+            [
+                'is_pagebuilder_enabled' => 1,
+                'is_html_allowed_on_front' => 1,
+                'is_wysiwyg_enabled' => 1
+            ]
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(Category::ENTITY, 'custom');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
