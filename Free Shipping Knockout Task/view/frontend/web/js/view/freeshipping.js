f9587e36c4b7e5fc9aeec36c6325264ebecd4f9f
Prajwal-Joshi-180 <94516750+Prajwal-Joshi-180@users.noreply.github.com>
2025-03-15 12:24:44 +0530

    Add files via upload

--- Free Shipping Knockout Task/view/frontend/web/js/view/freeshipping.js
+++ Free Shipping Knockout Task/view/frontend/web/js/view/freeshipping.js
@@ -1,34 +1,55 @@
-define(
-    [
-        'ko',
-        'uiComponent',
-        'Magento_Checkout/js/model/quote'
-    ],
-    function (ko, Component, quote) {
-        "use strict";
-        var freeshippingminimumamount = window.freeshippingamount;
-        var currency = window.currency;
-        return Component.extend({
-            defaults: {
-                template: 'Codilar_Sales/freeshipping'
-            },
-            getFreeShippingConfig: function() {
-                var totals = quote.totals();
-                var subtotalAmount = (totals ? totals : quote)['subtotal'];
-                if(freeshippingminimumamount && freeshippingminimumamount - subtotalAmount >0){
-                    let width = (subtotalAmount/freeshippingminimumamount)*100;
-                    document.getElementById('freeshiping-progress-bar-item').style.width = width + '%';
-                    return 'Shop '+ currency +(freeshippingminimumamount-subtotalAmount)+' More to Avail Free Shipping';
-                }
-                else {
-                    document.getElementById('freeshiping-progress-bar-item').style.width = '100%';
-                    document.getElementById('freeshiping-progress-bar-item').style.backgroundColor = 'green';
-                    return 'Your Order is eligible for Free Shippping';
-                }
-            },
-            getfreeshippingAmount: function (){
-                return freeshippingminimumamount;
+define([
+    'jquery',
+    'ko',
+    'uiComponent',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Customer/js/customer-data'
+], function ($, ko, Component, quote, customerData) {
+    "use strict";
+
+    var freeshippingminimumamount = window.freeshippingamount;
+    var currency = window.currency;
+    var cartData = customerData.get('cart');
+
+    return Component.extend({
+        defaults: {
+            template: 'Codilar_Sales/freeshipping'
+        },
+        freeshipping: ko.observable(),
+        progressBarWidth: ko.observable('100%'),
+        progressBarColor: ko.observable('green'),
+
+        initialize: function () {
+            this._super();
+
+            cartData.subscribe(function () {
+                this.getFreeShippingConfig();
+            }, this);
+
+            this.getFreeShippingConfig();
+        },
+        getFreeShippingConfig: function () {
+            var cart = customerData.get('cart');
+            var subtotalAmount = cart().subtotalAmount;
+
+            if (subtotalAmount <= 0) {
+                window.location.reload();
+            } else if (freeshippingminimumamount && (freeshippingminimumamount - subtotalAmount) > 0) {
+                let width = (subtotalAmount / freeshippingminimumamount) * 100;
+                this.progressBarWidth(width + '%');
+                this.progressBarColor('#ff5501');
+                this.freeshipping('Shop ' + currency + (freeshippingminimumamount - subtotalAmount) + ' More to Avail Free Shipping');
+            } else {
+                this.progressBarWidth('100%');
+                this.progressBarColor('green');
+                this.freeshipping('Your Order is eligible for Free Shipping');
             }
-        });
-    }
-);
+        },
+        getfreeshippingAmount: function () {
+            return freeshippingminimumamount;
+        },
+        getFreeshipping: function () {
+            return this.freeshipping;
+        }
+    });
+});
